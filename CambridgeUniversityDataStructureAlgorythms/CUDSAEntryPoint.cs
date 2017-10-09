686918cffe01c54048beb4a3ec7cb639aa835565
Oleksiy Izotov <oleksiy.izotov@sephora.com>
2017-10-08 19:43:31 -0700

    Chapter 1 excercise 3  - compare 1 000 000 items from arraylist and collection

--- CambridgeUniversityDataStructureAlgorythms/CUDSAEntryPoint.cs
+++ CambridgeUniversityDataStructureAlgorythms/CUDSAEntryPoint.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Basic;
+using System;
+using System.Collections;
 
 namespace CambridgeUniversityDataStructureAlgorythms
 {
@@ -7,16 +9,24 @@ namespace CambridgeUniversityDataStructureAlgorythms
         static void Main(string[] args)
         {
             //IntroCollectionsGenerics.IntroCollectionGenericHelper.BasicOperationsCollection();
-            IntroCollectionsGenerics.IntroCollectionGenericHelper.BasicOperationsCollectionExercise();
+            //IntroCollectionsGenerics.IntroCollectionGenericHelper.BasicOperationsCollectionExercise();
             //IntroCollectionsGenerics.IntroCollectionGenericHelper.DoSwap();
             //Exercises.c1.l1.ExamLauncher.Launch();
-            //long[] arr = new long[100000];
-            //arr = IntroCollectionsGenerics.IntroCollectionGenericHelper.BuildArray(arr);
-            //Timing.Timing tObj = new Timing.Timing();
-            //tObj.StartTime();
-            //IntroCollectionsGenerics.IntroCollectionGenericHelper.GetArray(arr);
-            //tObj.StopTime();
-            //Console.WriteLine("Totally {0} seconds ", tObj.GetDuration().Seconds);
+            ArrayList arr1 = new ArrayList();
+            UserCollection arr2 = new UserCollection();
+            arr1 = Exercises.c1.l3.Helper.BuildArray(arr1);
+            arr2 = Exercises.c1.l3.Helper.BuildArray(arr2);
+
+            Timing.Timing tObj = new Timing.Timing();
+            tObj.StartTime();
+            Exercises.c1.l3.Helper.GetArray(arr1);
+            tObj.StopTime();
+            Console.WriteLine("Totally {0} seconds ", tObj.GetDuration().Seconds);
+            Console.ReadLine();
+            tObj.StartTime();
+            Exercises.c1.l3.Helper.GetArray(arr2);
+            tObj.StopTime();
+            Console.WriteLine("Totally {0} seconds ", tObj.GetDuration().Seconds);
 
             Console.ReadLine();
         }
---
+++ Exercises/c1/l3/Helper.cs
@@ -0,0 +1,38 @@
+﻿using Basic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.c1.l3
+{
+    public static class Helper
+    {
+        public static ArrayList BuildArray(ArrayList arr)
+        {
+            for (long i = 0; i < 1000000; i++) arr.Add(i);
+
+            return arr;
+        }
+
+        public static void GetArray(ArrayList arr)
+        {
+            foreach(long i in arr) Console.Write(string.Concat(i, " "));
+        }
+
+        public static UserCollection BuildArray(UserCollection arr)
+        {
+            for (long i = 0; i < 1000000; i++) arr.Add(i);
+
+            return arr;
+        }
+
+        public static void GetArray(UserCollection arr)
+        {
+            foreach (long i in arr) Console.Write(string.Concat(i, " "));
+        }
+    }
+}
