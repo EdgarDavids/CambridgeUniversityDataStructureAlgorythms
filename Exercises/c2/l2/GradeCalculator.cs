dbcd0e67f08708215d6eafeec301a44a15f8375c
Oleksiy Izotov <oleksiy.izotov@sephora.com>
2017-10-15 19:00:03 -0700

    Chapter 2 Exercise 2 - grades of classes - average, highest, lowest. Based on array. Development for multi classes (2 dimention array)

---
+++ Exercises/c2/l2/GradeCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.c2.l2
+{
+    public static class GradeCalculator
+    {
+        public static void Calculate() {
+            Grade[] grades = new Grade[] {
+                new Grade(18, 39, 31, 10, 15, 20, 17),
+                new Grade(18, 53, 21, 10, 15, 20, 17)
+            };
+
+            Console.WriteLine("Grade calculations for 18 39 31 10 15 20 17");
+            Console.WriteLine("Grade calculations for 18 53 21 10 15 20 17");
+
+            foreach (Grade grade in grades)
+            {
+                Console.WriteLine("Average: {0}", grade.Average());
+                Console.WriteLine("Highest: {0}", grade.Highest());
+                Console.WriteLine("Lowest: {0}", grade.Lowest());
+            }
+
+        }
+    }
+}
