978a59b4bc2e38df2bdd9777f632caa127d0f9e0
Oleksiy Izotov <oleksiy.izotov@sephora.com>
2017-11-05 10:38:01 -0800

    User Queue

---
+++ Basic/UserQueue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    public class UserQueue
+    {
+        private ArrayList arrayList;
+
+        public UserQueue() {
+            arrayList = new ArrayList();
+        }
+
+        public void Enqueue(object item) {
+            arrayList.Add(item);
+        }
+
+        public void Dequeue() {
+            arrayList.RemoveAt(0);
+        }
+
+        public object Peek() {
+            return arrayList[0];
+        }
+
+        public void Clear() {
+            arrayList.Clear();
+        }
+
+        public int Count {
+            get { return arrayList.Count; }
+        }
+    }
+}
