8a239135ca7d141bc6ea89e1f99626b91a3ad4b4
Oleksiy Izotov <oleksiy.izotov@sephora.com>
2017-11-05 15:33:11 -0800

    Emergency - Priority Queue

--- Basic/Algorythms/QueueEmergency.cs
+++ Basic/Algorythms/QueueEmergency.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,51 @@ using System.Threading.Tasks;
 
 namespace Basic.Algorythms
 {
-    class QueueEmergency
+    public static class QueueEmergency
     {
+
+        public struct PQItem
+        {
+            public int priority;
+            public string name;
+        }
+
+        public class PQueue : Queue
+        {
+            public override object Dequeue()
+            {
+                object[] items;
+                int toppriority, minindex = 0;
+                items = this.ToArray();
+
+                toppriority = ((PQItem)items[0]).priority;
+                for (int i = 0; i < items.Length; i++) {
+                    if (((PQItem)items[i]).priority < toppriority) {
+                        toppriority = ((PQItem)items[i]).priority;
+                        minindex = i;
+                    }
+                }
+                this.Clear();
+                for (int i = 0; i < items.Length; i++) if (i != minindex) this.Enqueue(items[i]);
+                return items[minindex];
+            }
+
+        }
+        public static void Apply() {
+            PQueue erwait = new PQueue();
+            PQItem[] erPatient = new PQItem[4];
+            PQItem nextPatient;
+            erPatient[0].name = "Joe Smith";
+            erPatient[0].priority = 1;
+            erPatient[1].name = "Mary Brown";
+            erPatient[1].priority = 0;
+            erPatient[2].name = "Sam Jones";
+            erPatient[2].priority = 3;
+            for (int x = 0; x < erPatient.Length; x++)
+                erwait.Enqueue(erPatient[x]);
+            nextPatient = (PQItem)erwait.Dequeue();
+            Console.WriteLine(nextPatient.name);
+        }
+        
     }
 }
--- CambridgeUniversityDataStructureAlgorythms/CUDSAEntryPoint.cs
+++ CambridgeUniversityDataStructureAlgorythms/CUDSAEntryPoint.cs
@@ -14,7 +14,7 @@ namespace CambridgeUniversityDataStructureAlgorythms
             //Console.WriteLine("Min value is {0}", search.GetMin());
             //Console.WriteLine("Max value is {0}", search.GetMax());
 
-            Basic.Algorythms.RadixSort.Sort();
+            Basic.Algorythms.QueueEmergency.Apply();
 
             Console.ReadLine();
 
